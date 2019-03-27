17062b677b29b788cbd4a6a41d6d478e216bde17
jaejunha <dreamline91@naver.com>
2019-03-28 08:18:49 +0900

    treat ALU in control unit

--- controlunit.v
+++ controlunit.v
@@ -1,6 +1,6 @@
 `include "header.v"
 
-module ControlUnit(clk, reset_n, inst, PVSWriteEn);
+module ControlUnit(clk, reset_n, inst, PVSWriteEn, ALUSrcA, ALUSrcB, ALUOp, carry);
 	input clk;
 	input reset_n;
 	input [`WORD_SIZE-1:0] inst;
@@ -10,11 +10,37 @@ module ControlUnit(clk, reset_n, inst, PVSWriteEn);
 	reg [`OP_SIZE-1:0] op;
 	reg [`FUNC_SIZE-1:0] func;
 
-	parameter IF=0, ID=1, EX=2, MEM=3, WB=4;
+	output reg [1:0] ALUSrcA, ALUSrcB;
+	output reg [3:0] ALUOp;
+	output reg carry;
+
+	parameter IF = 0, ID = 1, EX = 2, MEM = 3, WB = 4;
 
 	initial
 		begin
 			op = inst[15:12];
 			func = inst[15:12];
+		end
+
+	always @(op or func)
+		begin
+			if(op == 4'd15)
+				begin
+					carry = 0;
+					case(func)
+						0: ALUOp = 4'b0000;	// ADD
+						1: ALUOp = 4'b0001;	// SUB
+						2: ALUOp = 4'b1101;	// AND
+						3: ALUOp = 4'b1110;	// OR
+						4: ALUOp = 4'b1100;	// NOT
+						5:			// TCP
+							begin
+								ALUOp = 4'b1100;
+								carry = 1;
+							end
+						6: ALUOp = 4'b0101;	// ALS
+						7: ALUOp = 4'b0100;	// ARS
+					endcase
+				end
 		end
 endmodule
--- cpu.v
+++ cpu.v
@@ -16,6 +16,10 @@ module cpu(clk, reset_n, readM, writeM, address, data, num_inst, output_port, is
 
 	wire PVSWriteEn;
 
+	wire [1:0] ALUSrcA, ALUSrcB;
+	wire [3:0] ALUOp;
+	wire carry;
+
 	Datapath datapath(clk, reset_n);
-	ControlUnit controlUnit(clk, reset_n, inst, PVSWriteEn);
+	ControlUnit controlUnit(clk, reset_n, inst, PVSWriteEn, ALUSrcA, ALUSrcB, ALUOp, carry);
 endmodule
